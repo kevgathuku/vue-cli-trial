b29246ad327a22a94b1de42ce13a403dd93eda0d
Kevin Ndung'u <kevgathuku@gmail.com>
2018-02-01 00:07:35 +0300

    Laracasts tabs + Bulma integration

---
+++ src/components/BulmaTabs.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="tabs is-centered">
+            <ul>
+                <li v-for="tab in tabs" :key="tab.name" :class="{'is-active': tab.isActive}">
+                    <a :href="tab.href" @click="selectTab(tab)">
+                        {{tab.name}}
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="tabs-details">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import TabPanel from "@/components/TabPanel.vue";
+
+export default {
+  name: "BulmaTabs",
+  components: {
+    "tab-panel": TabPanel
+  },
+
+  data() {
+    return {
+      tabs: []
+    };
+  },
+
+  created() {
+    this.tabs = this.$children;
+  },
+
+  methods: {
+    selectTab(selectedTab) {
+        this.tabs.forEach(tab => {
+            tab.isActive = (tab.name == selectedTab.name);
+        })
+    }
+
+  }
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <BulmaTabs>
+      <tab-panel name="Pictures" :selected="true">
+        <p>Here's some pictures for you</p>
+      </tab-panel>
+      <tab-panel name="Music">
+        <p>Here's some music for you</p>
+      </tab-panel>
+      <tab-panel name="Videos">
+        <p>Here's some videos for you</p>
+      </tab-panel>
+      <tab-panel name="Documents">
+        <p>Here's some documents for you</p>
+      </tab-panel>
+    </BulmaTabs>
   </div>
 </template>
+
+<script>
+import BulmaTabs from "@/components/BulmaTabs.vue";
+import TabPanel from "@/components/TabPanel.vue";
+
+export default {
+  components: {
+    "tab-panel": TabPanel,
+    BulmaTabs
+  }
+}
+</script>
